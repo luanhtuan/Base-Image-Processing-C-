db2aa7654eae3023a567dcafc9009deee809c649
Lu Anh Tuan <l.a.tvfx@gmail.com>
2019-09-22 22:46:17 +0700

    Update menu.py

--- Nuke_Custom_Tools/LensFlare_RND/menu.py
+++ Nuke_Custom_Tools/LensFlare_RND/menu.py
@@ -1,1 +1,5 @@
 
+myGizmosMenu = nuke.menu('Nuke').addMenu('[LuAnhTuan]/LensFlare')
+myGizmosMenu.addCommand('Horizol_Lens', "nuke.createNode('Horizol_Lens')")
+myGizmosMenu.addCommand('Spark_Lens', "nuke.createNode('Spark_Lens')")
+myGizmosMenu.addCommand('Galaxy_Lens', "nuke.createNode('Galaxy_Lens')")
