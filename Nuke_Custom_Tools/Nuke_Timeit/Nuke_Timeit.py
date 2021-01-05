ebe7d964cd20666a5fee7d7cb44e074dc6f97fbf
Lu Anh Tuan <l.a.tvfx@gmail.com>
2021-01-05 14:50:03 +0700

    Update Nuke_Timeit.py

--- Nuke_Custom_Tools/Nuke_Timeit/Nuke_Timeit.py
+++ Nuke_Custom_Tools/Nuke_Timeit/Nuke_Timeit.py
@@ -1,1 +1,70 @@
 
+# Add Timeit - STEP 1
+
+import random
+import  timeit
+
+txt = nuke.getInput('how many objects', 'type value')
+list = []
+for i in range(int(txt)):
+    a = nuke.nodes.Sphere()
+    posx = float(float(i) * float(random.uniform(1,3)))
+    posy = float(random.uniform(2.5,5))
+    rndNum = timeit.timeit('"-".join(str(i) for i in range (100))',
+                   number = 10000)
+    a['translate'].setValue([posx,posy,0])
+    a['translate'].setExpression('sin((frame+1000)/'+str(rndNum)+')'+'*'+str(rndNum*2),2)
+    a['uniform_scale'].setValue(0.5)
+    a.setSelected(True)
+    k = nuke.selectedNodes()
+b = nuke.createNode("Scene")
+x = 0
+for i in k:
+  b.setInput(x,i)
+
+# Add Timeit - STEP 2
+
+
+import random
+import  timeit
+
+txt = nuke.getInput('how many objects', 'type value')
+list = []
+for i in range(int(txt)):
+    a = nuke.nodes.Sphere()
+    posx = float(float(i) * float(random.uniform(1,3)))
+    posy = float(random.uniform(2.5,5))
+    rndNum = timeit.timeit('"-".join([str(i) for i in range (100)])',
+                            number = 10000)
+    a['translate'].setValue([posx,posy,0])
+    a['translate'].setExpression('sin((frame+1000)/'+str(rndNum)+')'+'*'+str(rndNum*2),2)
+    a['uniform_scale'].setValue(0.5)
+    a.setSelected(True)
+    k = nuke.selectedNodes()
+b = nuke.createNode("Scene")
+x = 0
+for i in k:
+  b.setInput(x,i
+
+# Add Timeit - STEP 3
+
+  import random
+  import timeit
+
+  txt = nuke.getInput('how many objects', 'type value')
+  list = []
+  for i in range(int(txt)):
+      a = nuke.nodes.Sphere()
+  posx = float(float(i) * float(random.uniform(1, 3)))
+  posy = float(random.uniform(2.5, 5))
+  rndNum = timeit.timeit('"-".join(map(str, range(100)))',
+                         number=10000)
+  a['translate'].setValue([posx, posy, 0])
+  a['translate'].setExpression('sin((frame+1000)/' + str(rndNum) + ')' + '*' + str(rndNum * 2), 2)
+  a['uniform_scale'].setValue(0.5)
+  a.setSelected(True)
+  k = nuke.selectedNodes()
+  b = nuke.createNode("Scene")
+  x = 0
+  for i in k:
+      b.setInput(x, i)
